2716f0f397d781bdb53b18020ef4f80eda7ead65
Serban Teodorescu <teodorescu.serban@gmail.com>
2015-11-23 22:41:00 +0200

    multiple changes; removed docker-compose related files. close to finish.

---
+++ api/init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /srv/bas-api
+
+function setup_config() {
+    echo "DATABASE = {"                         >  config.py
+    echo "    'hostname': '${MYSQL_HOST}',"     >> config.py
+    echo "    'database': '${MYSQL_DB}',"       >> config.py
+    echo "    'port': ${MYSQL_PORT},"           >> config.py
+    echo "    'username': '${MYSQL_USER}',"     >> config.py
+    echo "    'password': '${MYSQL_PASS}',"     >> config.py
+    echo "}"                                    >> config.py
+}
+
+# setup the config.py
+echo "Ensure config file is correct..."
+[ -f config.py ] || setup_config
+echo "Done."
+
+# make sure we have something to run
+echo "Ensure wsgi file is correctly configured..."
+[ -f bas-api.py ] || cp -a bas-api.wsgi.TEMPLATE bas-api.py
+echo "Done."
+
+# import schema
+echo "Warning! All tables will be wiped out!"
+echo "Starting in 10 seconds. Hit Ctrl+C to abort..."
+sleep 10
+mysql -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DB} < sql/schema.sql
+echo "Done. Database is initialized."
+
+# run updates
+echo "Running updates."
+sh update-tenders.sh
+sh update-contracts.sh
+echo "Done."
--- build_it_locally.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for image in base base-mysql base-python mysql api; do
-
-    docker build -t docker.io/teodorescuserban/pspc-$image:latest $image/
-
-done
